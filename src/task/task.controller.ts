37ea68cac85707d64c6b4525311d39a486754efc
Victor Gustavo <victorgustavo.dev@gmail.com>
2025-09-07 16:50:33 -0300

    feat: implements findById and throw errors with status code

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,16 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { TaskDto } from './task.dto';
 import { TaskService } from './task.service';
 
 @Controller('task')
 export class TaskController {
 
-  constructor(private readonly taskService: TaskService) {
+  constructor(private readonly taskService: TaskService){}
 
-  }
 
   @Post()
   create(@Body() task: TaskDto) { 
     this.taskService.create(task);
+  }
+
+  @Get('/:id')
+  findById(@Param('id') id:string){
+    console.log(this.taskService.findById(id));
   }
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,13 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { TaskDto } from './task.dto';
 
 @Injectable()
 export class TaskService {
 
-    private task: TaskDto[] = [];
+    private tasks: TaskDto[] = [];
 
     create(task: TaskDto) {
-        this.task.push(task);
-        console.log(task);
+        this.tasks.push(task);
+        console.log(this.tasks);
+    }
+
+    findById(id: string): TaskDto {
+        const foundTask = this.tasks.filter(t => t.id === id);
+
+        if(foundTask.length) {
+            return foundTask[0];
+        }
+
+        throw new HttpException(`Task id: ${id}, not found!`, HttpStatus.NOT_FOUND);
     }
 }
